05b2697f3a990f443a2c52061a45069f79560cdc
Petter Karlsrud <petterkarlsrud@me.com>
2018-12-05 23:32:44 +0100

    Refactor: Rewrote the application

--- tags.go
+++ tags.go
@@ -9,6 +9,31 @@ import (
 // TagName is the name of the tag used to annotate gojq queries
 const TagName = "tags"
 
+func queryJSON(data, query string) (interface{}, error) {
+	parser, err := gojq.NewStringQuery(data)
+	if err != nil {
+		return nil, err
+	}
+
+	result, err := parser.Query(query)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, err
+}
+
+func setField(fieldNr int, value interface{}, instance *reflect.Value) error {
+	switch value.(type) {
+	case string:
+		instance.Elem().Field(fieldNr).SetString(value.(string))
+	case float64:
+		instance.Elem().Field(fieldNr).SetInt(int64(value.(float64)))
+	}
+
+	return nil
+}
+
 // Parse parses a reflect.Type
 func Parse(t reflect.Type, data string) (value reflect.Value, err error) {
 	value = reflect.New(t)
@@ -19,21 +44,14 @@ func Parse(t reflect.Type, data string) (value reflect.Value, err error) {
 			continue
 		}
 
-		parser, err := gojq.NewStringQuery(data)
+		result, err := queryJSON(data, tagValue)
 		if err != nil {
 			return value, err
 		}
 
-		result, err := parser.Query(tagValue)
+		err = setField(i, result, &value)
 		if err != nil {
 			return value, err
-		}
-
-		switch result.(type) {
-		case string:
-			value.Elem().Field(i).SetString(result.(string))
-		case float64:
-			value.Elem().Field(i).SetInt(int64(result.(float64)))
 		}
 	}
 	return value, err
